615d5ae4e8afc2958fc688ebfc7ff8f7359fbc2e
香饽饽zizizi <863461783@qq.com>
2018-11-14 11:50:37 +0800

    remove: qiniu

--- src/config/dev.config.js
+++ src/config/dev.config.js
@@ -10,12 +10,6 @@ export default {
     expiresIn: 1000 * 60 * 60 * 24,
     clientPrefix: 'Bearer'
   },
-  QINIU: {
-    accessKey: 'vatLY77qxBYd0SyeqNuDANZT2SL_PnePyLlLefrL',
-    secretKey: 'DUxsP7JCdynq7VakoTzyYH3AH2DsTjOKu2r1sfwe',
-    bucketName: 'xbb-blog-test',
-    expires: 7200
-  },
   ADMIN: {
     username: '13800000000',
     password: 'dwyane3wade'
--- src/route/index.js
+++ src/route/index.js
@@ -3,7 +3,6 @@ import auth from './auth'
 import tag from './tag'
 import category from './category'
 import article from './article'
-import qiniu from './qiniu'
 
 const router = new Router({
   prefix: '/api'
@@ -14,6 +13,5 @@ router
   .use('/tag', tag.routes())
   .use('/category', category.routes())
   .use('/article', article.routes())
-  .use('/qiniu', qiniu.routes())
 
 export default router
