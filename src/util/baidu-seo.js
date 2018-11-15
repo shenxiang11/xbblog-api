2aaf1611b6a4a365baae5ee138edf9623abc36cf
香饽饽zizizi <863461783@qq.com>
2018-11-15 14:41:07 +0800

    fix

--- src/util/baidu-seo.js
+++ src/util/baidu-seo.js
@@ -1,5 +1,4 @@
 import conf from '../config'
-import consola from 'consola'
 import request from 'request'
 
 const postRequest = ({ urlKey, urls, msg }) => {
@@ -11,27 +10,27 @@ const postRequest = ({ urlKey, urls, msg }) => {
 		headers: { 'Content-Type': 'text/plain' },
 		url: `http://data.zz.baidu.com/${urlKey}?site=${conf.BAIDUSEO.site}&token=${conf.BAIDUSEO.token}`
 	}, (error, response, body) => {
-    consola.error(error)
-    consola.log('response:', response)
-    consola.log('body:', body)
-    consola.log('=======================')
+    console.error(error)
+    console.log('response:', response)
+    console.log('body:', body)
+    console.log('=======================')
 	})
 }
 
 // 提交记录
 exports.baiduSeoPush = urls => {
-  consola.log('百度推送：', urls)
+  console.log('百度推送：', urls)
 	postRequest({ urls, urlKey: 'urls', msg: '百度推送结果：' })
 }
 
 // 更新记录
 exports.baiduSeoUpdate = urls => {
-  consola.log('百度更新：', urls)
+  console.log('百度更新：', urls)
 	postRequest({ urls, urlKey: 'update', msg: '百度更新结果：' })
 }
 
 // 删除记录
 exports.baiduSeoDelete = urls => {
-  consola.log('百度删除：', urls)
+  console.log('百度删除：', urls)
 	postRequest({ urls, urlKey: 'del', msg: '百度删除结果：' })
 }
